e46e13833d2dfbda6318d54f8c6f9584eb9d539e
Pusit20574 <pusit.20574@mail.kmutt.ac.th>
2021-02-21 00:16:42 +0700

    Add groupwork4

--- 62130500076_groupwork_3/main.js
+++ 62130500076_groupwork_3/main.js
@@ -1,17 +1,45 @@
 const app = {
     data() {
         return {
-            // heart: [{heart: './images/heart.png', done:false}],
             allPic: [{url: './images/1.jpg',name:'Firstday, Canada', done: false},
                      {url: './images/2.jpg',name:'Midnight, Australia', done: false},
                      {url: './images/3.jpg',name:'Evening, Newzealand', done: false}],
-            
+            isHidden: true,
+            searchName: '',
+            enterSearchName: '',
+            noResult: false,
+            expandPhoto: false,
+            photoIndex: ''
         }
     },
     methods:{
         showFavIcon(index){
-            this.allPic[index].done = !this.allPic[index].done;
+            if(this.resultQuery.length == 2){
+                this.allPic[index++];
+                this.allPic[index].done = !this.allPic[index].done;          
+            }else{
+                this.allPic[index].done = !this.allPic[index].done;     
+            }
+                 
+        },
+        doSearch(){
+            this.enterSearchName = this.searchName;
+            if(this.resultQuery.length == 0){
+                this.noResult = true
+            }else { this.noResult = false; }
+        },
+        cancel(){
+            this.isHidden = true;
+            this.searchName = this.enterSearchName = '';
+            this.noResult = false;
+        },
+        expandPic(index){
+            if(!this.expandPhoto){
+                this.photoIndex = index;
+            }
+            this.expandPhoto = !this.expandPhoto;
         }
+        
     },
     computed:{
         countLike(){
@@ -19,7 +47,13 @@ const app = {
         },
         countShow(){
             return this.allPic.length;
+        },
+        resultQuery() {
+            return this.allPic.filter((i) => {
+                return i.name.toLowerCase().includes(this.enterSearchName.toLowerCase());
+            })        
         }
+           
     }
 }
 
