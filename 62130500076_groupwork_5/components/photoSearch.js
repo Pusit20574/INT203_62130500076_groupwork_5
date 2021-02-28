8dd3c089cb024f09812b039dc75fa972349e0e18
Pusit20574 <pusit.20574@mail.kmutt.ac.th>
2021-02-28 19:54:57 +0700

    Update fix bugs

--- 62130500076_groupwork_5/components/photoSearch.js
+++ 62130500076_groupwork_5/components/photoSearch.js
@@ -1,6 +1,6 @@
 app.component('photo-search', {
     props: {
-        'no-photo': Boolean
+        'no-result': Boolean
     },
     data() {
             tempSearchName: '';
@@ -8,11 +8,13 @@ app.component('photo-search', {
     template:
         /*html*/
     `
-    <div class="flex ml-20" v-show = "!showSearch">
-        <!-- search input form -->
-        <input @input="addSearchName" v-model="tempSearchName" @keyup.enter="$emit('do-search')" type="text" placeholder="Enter name of Photo" class="mt-10 ml-12 bg-purple-white shadow rounded border-0 p-3">
-        <!-- cancel button -->
-        <button @click = "deleteSearchName; $emit('cancel-button')" class="ml-10 mt-10 text-md bg-white hover:bg-gray-200 text-gray-700 font-mono font-medium py-2 px-4 border border-gray-400 rounded shadow">cancel</button>
+    <div class = "ml-20">
+        <div class="flex">
+            <!-- search input form -->
+            <input @input="addSearchName" v-model="tempSearchName" @keyup.enter="$emit('do-search')" type="text" placeholder="Enter name of Photo" class="mt-10 ml-12 bg-purple-white shadow rounded border-0 p-3">
+            <!-- cancel button -->
+            <button @click = "deleteSearchName" @click = "$emit('cancel-button')" class="ml-10 mt-10 text-md bg-white hover:bg-gray-200 text-gray-700 font-mono font-medium py-2 px-4 border border-gray-400 rounded shadow">cancel</button>
+        </div>
         <!-- text No Photos -->
         <p v-show = "noResult" class="ml-20 mt-10 font-mono text-red-400 text-4xl">X No Photos</p>
     </div>
